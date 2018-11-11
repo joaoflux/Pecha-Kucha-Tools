ca17495aa899eee8cf0b3d1d316c4df21c2b831b
Joachim Stein <joachim.stein@gmail.com>
2018-11-11 15:43:33 +0100

    initial commit

---
+++ event/01/_config.js
@@ -0,0 +1,6 @@
+var speaker = "Asmus Trautsch";
+var topic = "Der See - ich sah";
+var date = "19.10.2018";
+var delay_in = "";
+var delay_out = "0";
+var link = "";
---
+++ event/create/index.php
@@ -0,0 +1,1 @@
+<?php include '../../application/create-event.php'; ?>
